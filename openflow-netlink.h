637130e1802c4bc458ad7c70671085e2258435aa
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2026-06-02 16:14:16 +0330

    Update openflow-netlink.h

--- openflow-netlink.h
+++ openflow-netlink.h
@@ -1,7 +1,6 @@
 /* Copyright (c) 2008 The Board of Trustees of The Leland Stanford
  * Junior University
  * 
-
  * We are making the OpenFlow specification and associated documentation
  * (Software) available for public use and benefit with the expectation
  * that others will use, modify and enhance the Software and contribute
